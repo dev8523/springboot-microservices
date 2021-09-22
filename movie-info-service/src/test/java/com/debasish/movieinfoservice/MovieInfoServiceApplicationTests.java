c97e6e6dee461cbc97afed9d0a65253de8eff9a0
dev8523 <debasishsahoo8523@gmail.com>
2021-09-22 23:48:17 +0530

    baseline setup for eureka client

--- movie-info-service/src/test/java/com/debasish/movieinfoservice/MovieInfoServiceApplicationTests.java
+++ movie-info-service/src/test/java/com/debasish/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.debasish.movieinfoservice;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
