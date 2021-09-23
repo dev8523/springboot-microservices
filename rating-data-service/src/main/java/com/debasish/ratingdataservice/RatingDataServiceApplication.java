6c9fb3694a01b3f7378205772347bf20c5a0c922
dev8523 <debasishsahoo8523@gmail.com>
2021-09-23 20:46:37 +0530

    Add eureka client dependency, Enable eureka client in main class, and change the application name

--- rating-data-service/src/main/java/com/debasish/ratingdataservice/RatingDataServiceApplication.java
+++ rating-data-service/src/main/java/com/debasish/ratingdataservice/RatingDataServiceApplication.java
@@ -2,8 +2,10 @@ package com.debasish.ratingdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingDataServiceApplication {
 
 	public static void main(String[] args) {
