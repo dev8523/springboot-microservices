bc562dfed25c8daa280661bb426bad10421176aa
dev8523 <debasishsahoo8523@gmail.com>
2021-09-23 21:22:39 +0530

    Run junit successfully for creating jar

--- movie-info-service/src/test/java/com/debasish/movieinfoservice/MovieInfoServiceApplicationTests.java
+++ movie-info-service/src/test/java/com/debasish/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -2,13 +2,16 @@ package com.debasish.movieinfoservice;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class MovieInfoServiceApplicationTests {
+public class MovieInfoServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
