1a94234850014e86b5c0d45fc30b2580f8b2ff4b
dev8523 <debasishsahoo8523@gmail.com>
2021-09-28 00:20:45 +0530

    Set 3sec timeout to RestTemplate call for Fault Tolerance and Resilience

--- movie-catalog-service/src/main/java/com/debasish/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/debasish/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -17,17 +18,23 @@ public class MovieCatalogServiceApplication {
 
     /**
      * Description for @Bean : Spring is gonna execute this method  whatever you return, it is gonna say hey RestTemplate maps to this instance.
-     * Anybody autowires RestTemplate they get this one instance. This method executes just once.
-     *
+     * Anybody autowires RestTemplate they get this one instance. This method executes only once.
+     * <p>
      * Description for @LoadBalanced : It does the service discovery in a load balanced way. Basically we are telling RestTemplate
      * the URL we will be providing is just a hint about what service you need to discover.
+     * <p>
+     * Setting Timeout : Wih this RestTemplate, As long as the response comes back within 3 secs it's all good. If response doesn't come back
+     * it is going to throw an error.
+     * <p>
      *
      * @return RestTemplate - returns a RestTemplate instance
      */
     @Bean
     @LoadBalanced
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(3000); // Timeout has been set to 3 sec.
+        return new RestTemplate(clientHttpRequestFactory);
     }
 
 	/*@Bean
