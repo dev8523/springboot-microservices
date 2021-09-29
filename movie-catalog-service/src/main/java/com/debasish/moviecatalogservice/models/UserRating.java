1827bebb1f9468ba7f547a2b7530013334a87035
dev8523 <debasishsahoo8523@gmail.com>
2021-09-30 01:12:14 +0530

    Add userId field to UserRating class

--- movie-catalog-service/src/main/java/com/debasish/moviecatalogservice/models/UserRating.java
+++ movie-catalog-service/src/main/java/com/debasish/moviecatalogservice/models/UserRating.java
@@ -4,13 +4,23 @@ import java.util.List;
 
 public class UserRating {
 
+    private String userId;
     private List<Rating> userRating;
 
     public UserRating() {
     }
 
-    public UserRating(List<Rating> userRating) {
+    public UserRating(String userId, List<Rating> userRating) {
+        this.userId = userId;
         this.userRating = userRating;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public List<Rating> getUserRating() {
