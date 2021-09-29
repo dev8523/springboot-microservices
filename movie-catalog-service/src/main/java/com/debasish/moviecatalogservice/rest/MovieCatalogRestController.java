fb629eda0abf23da3bbbcec6f724df03c8ef65d6
dev8523 <debasishsahoo8523@gmail.com>
2021-09-30 01:17:50 +0530

    Add fallback methods for each rest calls having separate methods. But this will not work due to proxy in spring.

--- movie-catalog-service/src/main/java/com/debasish/moviecatalogservice/rest/MovieCatalogRestController.java
+++ movie-catalog-service/src/main/java/com/debasish/moviecatalogservice/rest/MovieCatalogRestController.java
@@ -2,6 +2,7 @@ package com.debasish.moviecatalogservice.rest;
 
 import com.debasish.moviecatalogservice.models.CatalogItem;
 import com.debasish.moviecatalogservice.models.Movie;
+import com.debasish.moviecatalogservice.models.Rating;
 import com.debasish.moviecatalogservice.models.UserRating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,34 +33,70 @@ public class MovieCatalogRestController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
-    /*@Autowired
-    private WebClient.Builder webClientBuilder;*/
+    @GetMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable(name = "userId") String userId) {
+
+        // It will map with the spring.application.name that we have provided in app.properties file.
+        UserRating userRating = getUserRating(userId);
+
+        // for each movie ID, call movie info service and get details
+        return userRating.getUserRating()
+                .stream()
+                .map(this::getCatalogItem)
+                .collect(Collectors.toList());
+    }
 
     /**
      * Description for @HystrixCommand : We are telling hystrix this is the method which shouldn't cause the whole thing to go down.
      * We want to break the circuit when something goes down with this endpoint.
      * <p>
-     * In any case there is a circuit breakdown, it will execute getFallbackCatalog() method as default.
+     * In any case there is a circuit breakdown, it will execute getFallbackCatalogItem() method as default.
+     * <p>
+     *
+     * @param rating rating fields to be set.
+     * @return List<CatalogItem>
+     */
+    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+    private CatalogItem getCatalogItem(Rating rating) {
+        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class); // It will map with the spring.application.name that we have provided in app.properties file.
+        // Put them all together
+        assert movie != null;
+        return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+    }
+
+    /**
+     * This is the fallback method for getCatalogItem().
+     *
+     * @param rating
+     * @return CatalogItem
+     */
+    private CatalogItem getFallbackCatalogItem(Rating rating) {
+        return new CatalogItem("Movie Name Not Found", "NA", rating.getRating());
+    }
+
+    /**
+     * Description for @HystrixCommand : We are telling hystrix this is the method which shouldn't cause the whole thing to go down.
+     * We want to break the circuit when something goes down with this endpoint.
+     * <p>
+     * In any case there is a circuit breakdown, it will execute getFallbackUserRating() method as default.
      * <p>
      *
      * @param userId userId to be set.
      * @return List<CatalogItem>
      */
-    @GetMapping("/{userId}")
-    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
-    public List<CatalogItem> getCatalog(@PathVariable(name = "userId") String userId) {
+    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    private UserRating getUserRating(@PathVariable("userId") String userId) {
+        return restTemplate.getForObject("http://rating-data-service/ratingsdata/users/" + userId, UserRating.class);
+    }
 
-        // It will map with the spring.application.name that we have provided in app.properties file.
-        UserRating ratings = restTemplate.getForObject("http://rating-data-service/ratingsdata/users/" + userId, UserRating.class);
-
-        return ratings.getUserRating().stream().map(rating -> {
-            // for each movie ID, call movie info service and get details
-            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class); // It will map with the spring.application.name that we have provided in app.properties file.
-            // Put them all together
-            assert movie != null;
-            return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
-        }).collect(Collectors.toList());
-
+    /**
+     * This is the fallback method for getUserRating().
+     *
+     * @param userId
+     * @return UserRating
+     */
+    private UserRating getFallbackUserRating(@PathVariable("userId") String userId) {
+        return new UserRating(userId, Collections.singletonList(new Rating("0", 0)));
     }
 
     /**
@@ -73,13 +109,3 @@ public class MovieCatalogRestController {
         return Collections.singletonList(new CatalogItem("No Movie", "", 0));
     }
 }
-
-
-/*
-            Movie movie = webClientBuilder.build()
-                    .get()
-                    .uri("http://localhost:8083/ratingsdata/movies/" + rating.getMovieId())
-                    .retrieve()
-                    .bodyToMono(Movie.class)
-                    .block();
-*/
